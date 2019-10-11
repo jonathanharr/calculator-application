c46dc23693e32b1f3be3a9928e8129eaf8a74b50
jonathanharr92 <49676040+jonathanharr92@users.noreply.github.com>
2019-10-11 11:41:04 +0200

    Removed Arithmethic-core from the dependency declarations, it had already been removed from the project when I implemented my RESTFul API Service with Match functions. Added Lombok as a dependency since this project depended on Arithmetic-core's Lombok dependency. Works as of now in IntelliJ for me.

--- webmodule/src/main/java/se/calculatorprogram/WebMain.java
+++ webmodule/src/main/java/se/calculatorprogram/WebMain.java
@@ -1,6 +1,5 @@
 package se.calculatorprogram;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
